f80e7eb4479d8246adb0da5f84b178dbbc6dd7a7
Jack Zentner <jack.zentner@gmail.com>
2015-12-28 10:59:31 -0500

    updated to work with PyQt5, PySide2, PyQt4 and PySide

--- examples/viewer/vtkviewer.py
+++ examples/viewer/vtkviewer.py
@@ -1,23 +1,88 @@
 # Based on this example:  http://www.vtk.org/Wiki/VTK/Examples/Python/Widgets/EmbedPyQt2
 
+def __import_qt_modules():
+    """
+    Trys to import and then returns the necessary Qt related modules with a preference given to 
+    Qt5 over Qt4 and PySide over PyQt.  If no python Qt libraries are found, raises an
+    ImportError.
+    """
+    try:
+        from PySide2 import QtWidgets as  qt_gui
+        from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor as renderer
+        return qt_gui, renderer 
+    except ImportError:
+        pass
+    
+    try:
+        from PyQt5 import QtWidgets as  qt_gui
+        from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor as renderer
+        return qt_gui, renderer 
+    except ImportError:
+        pass
 
-from PySide import QtCore, QtGui
+    try:
+        from PySide import QtGui as  qt_gui
+        from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor as renderer
+        return qt_gui, renderer 
+    except ImportError:
+        pass
+
+    try:
+        from PyQt4 import QtGui as  qt_gui
+        from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor as renderer
+        return qt_gui, renderer 
+    except ImportError:
+        pass
+
+    raise ImportError("Could no import necessary modules. The VTK-OpenCASCADE viewer requires VTK and PySide2, PyQt5, PySide or PyQt4.")
+
 
 import vtk
-from vtk.qt4.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+qt_gui, vtk_renderer = __import_qt_modules()
+
+
+def get_QApplication(*args):
+    try:
+        from PySide2.QtWidgets import QApplication as qApp
+        return qApp(args)
+    except ImportError:
+        pass
+
+    try:
+        from PyQt5.QtWidgets import QApplication as qApp
+        return qApp(args)
+    except ImportError:
+        pass
+
+    try:
+        from PySide.QtGui import QApplication as qApp
+        return qApp(args)
+    except ImportError:
+        pass
+
+    try:
+        from PyQt4.QtGui import QApplication as qApp
+        return qApp(args)
+    except ImportError:
+        pass
+
+
+    raise ImportError("Could not create the QApplication object.")
+
+
  
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(603, 553)
-        self.centralWidget = QtGui.QWidget(MainWindow)
-        self.gridlayout = QtGui.QGridLayout(self.centralWidget)
-        self.vtkWidget = QVTKRenderWindowInteractor(self.centralWidget)
+        self.centralWidget = qt_gui.QWidget(MainWindow)
+        self.gridlayout = qt_gui.QGridLayout(self.centralWidget)
+        self.vtkWidget = vtk_renderer(self.centralWidget)
         self.gridlayout.addWidget(self.vtkWidget, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralWidget)
  
 
-class SimpleVtkViewer(QtGui.QMainWindow):
+class SimpleVtkViewer(qt_gui.QMainWindow):
     """
     SimpleVtkViewer uses a VTK QVTKRenderWindowInteractor to provide interactive
     rendeirng of VTK props in a QT window.  For keyboard and mouse interaction
@@ -29,7 +94,7 @@ class SimpleVtkViewer(QtGui.QMainWindow):
     """
     
     def __init__(self, parent = None):
-        QtGui.QMainWindow.__init__(self, parent)
+        qt_gui.QMainWindow.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ren = vtk.vtkRenderer()
